6086fc1e85661d6018c3c748041ea71cba8182c3
rickharris-dev <rickharris724@gmail.com>
2016-04-03 21:33:12 -0700

    Script maintains order of keys

--- discover_more_python/github_locations_sequential_2.py
+++ discover_more_python/github_locations_sequential_2.py
@@ -7,9 +7,14 @@ request_headers = {
   'Authorization': 'token 4d55fa9beb614fd08ed010f0442c5a7239e734ee'
 }
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc"
-
+array = []
 req = urllib2.Request(url, headers=request_headers)
 response = urllib2.urlopen(req)
 data = json.load(response)
 for i in data['items']:
-    print i['full_name']
+    user_url = i['owner']['url']
+    user_req = urllib2.Request(user_url, headers = request_headers)
+    user_response = urllib2.urlopen(user_req)
+    user_data = json.load(user_response)
+    array.append(dict([('full_name', i['full_name']), ('location', user_data['location'])]))
+print json.dumps(array, sort_keys=True)
