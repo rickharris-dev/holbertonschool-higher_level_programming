0a2fe97dbc1c740e5cad4391bec84eb30627b9e4
rickharris-dev <rickharris724@gmail.com>
2016-05-23 16:22:03 -0700

    Function returns the fibonacci number at the given index

---
+++ swift_code_is_so_fast/5-fibonnacci.swift
@@ -0,0 +1,20 @@
+// Function returns the fibonacci number at the given index
+func fibonacci(number: Int) -> (Int) {
+        var i:Int = 2
+        var a:Int = 1
+        var b:Int = 1
+        var c:Int = 0
+        if number == 0 {
+                return a
+        } else if number == 1 {
+                return b
+        } else {
+                while i < number {
+                        c = a
+                        a = b
+                        b = c + a
+                        i += 1
+                }
+                return b
+        }
+}
