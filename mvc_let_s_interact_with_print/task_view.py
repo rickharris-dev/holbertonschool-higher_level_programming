a8b19084e63bea400875ce9c9567f465288aa700
rickharris-dev <rickharris724@gmail.com>
2016-05-25 10:15:13 -0700

    Updated the constructor and fixed issue with label and button declaration

--- mvc_let_s_interact_with_print/task_view.py
+++ mvc_let_s_interact_with_print/task_view.py
@@ -1,14 +1,17 @@
 import Tkinter as tk
 
-class TaskView:
+class TaskView(tk.Toplevel):
 
     def __init__(self, master):
+        tk.Toplevel.__init__(self, master)
+        self.protocol('WM_DELETE_WINDOW', self.master.destroy)
+
         if not isinstance(master, tk.Tk):
             raise Exception("master is not a tk.Tk()")
 
         self.__title_var = tk.StringVar()
-        self.__title_label = tk.Label(master, textvariable=self.__title_var)
-        self.toggle_button = tk.Button(master, text="Reverse")
+        self.__title_label = tk.Label(self, textvariable=self.__title_var)
+        self.toggle_button = tk.Button(self, text="Reverse")
         self.toggle_button.pack(side='left')
 
     def update_title(self, title):
