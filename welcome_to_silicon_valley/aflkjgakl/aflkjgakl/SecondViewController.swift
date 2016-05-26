8c9a48ac2edf383b44b18226e5ecdea525235dcd
Richard Harris <38@p4-2.hbtn.io>
2016-05-26 16:41:44 -0700

    Creates table view of companies

---
+++ welcome_to_silicon_valley/aflkjgakl/aflkjgakl/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  aflkjgakl
+//
+//  Created by Richard Harris on 5/26/16.
+//  Copyright © 2016 Holberton School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
